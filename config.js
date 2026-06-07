23b312eff2e8bc82a49f0dfb2eef9b374d67b391
Avinash Nandakumar <31336290+avinashnandakumar@users.noreply.github.com>
2026-06-07 15:04:05 -0700

    Set snapshotUrl to the deployed Apps Script URL
    testing live sheets link

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.WORLD_CUP_CONFIG = {
   // Replace this with your deployed Apps Script Web App /snapshot URL when ready.
   // Leave as null to use the checked-in simulated sample data.
-  snapshotUrl: null
+  snapshotUrl: 'https://script.googleusercontent.com/macros/echo?user_content_key=AUkAhnQZwjESmRO9aIEIXVKVNQlhkEvCfiDuufKAguZvaSLC-U6Unt4o289gZm9J2OxK26hsXiIVrWjvmUDungOge1nukb0MonMFVgK0cfGtHv6Dzs5BXCcnQENjXTRbsPctIPlh1hPUOoREGq3_Lc9R2HfXgAg8hllV6EaIUnXL8qOPkGBxDmhbXKTy6aEorQUNA2u_mbjVbAZARQSVenIIeG0edu2I6GnbiJK8ddt_FtgfZQujmgCnjRnkoLjvTB2donJd5-9OnW24_aMjyHR_KhpgATuy6Q&lib=MQ_ESOm6cr3DfrI5-i3orCttWKBJahI15'
 };
